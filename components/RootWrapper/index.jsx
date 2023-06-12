fca281eec5b69ae3c4aa9a644385c589655d46d8
devRabbani <dev.golamrabbani@gmail.com>
2023-06-12 20:42:22 +0530

    👌 IMPROVE: added route guard and loading screen for authready

--- components/RootWrapper/index.jsx
+++ components/RootWrapper/index.jsx
@@ -1,11 +1,17 @@
 import useAuthCheck from '@/hooks/useAuthCheck'
 import useGetUserData from '@/hooks/useGetUserData'
-import { selectAuthReady } from '@/redux/features/authSlice'
+import {
+  selectAuthReady,
+  selectUser,
+  selectUserData,
+  selectUserDataLoading,
+} from '@/redux/features/authSlice'
 import { useSelector } from 'react-redux'
 import FullLoading from '../Loaders/fullLoading'
-import { Router } from 'next/router'
+import { Router, useRouter } from 'next/router'
 import nProgress from 'nprogress'
 import { useEffect } from 'react'
+import Layout from '../Layout'
 
 export default function RootWrapper({ children }) {
   //NProgress Loading Animation
@@ -27,15 +33,42 @@ export default function RootWrapper({ children }) {
 
   // Getting Auth STate Data
   const isAuthReady = useSelector(selectAuthReady)
+  const userDataLoading = useSelector(selectUserDataLoading)
+  const user = useSelector(selectUser)
+  const userData = useSelector(selectUserData)
 
   // Checking Auth
   useAuthCheck()
   useGetUserData()
 
-  // Loading Screen If AUth not ready yet
-  if (!isAuthReady) {
+  // Route Guarding
+  // Router
+  const router = useRouter()
+  const publicRoutes = ['/welcome', '/login']
+  const isPrivate = !publicRoutes.includes(router.pathname)
+
+  useEffect(() => {
+    // For Private Routes Routes
+    if (isPrivate) {
+      if ((!user && isAuthReady) || (!userDataLoading && !userData?.username)) {
+        router.push('/welcome') // Route to welcome page
+      }
+    }
+  }, [
+    router,
+    user,
+    userData?.username,
+    userDataLoading,
+    isAuthReady,
+    isPrivate,
+  ])
+
+  // Loading Screen If AUth not ready yet and Data Loading
+  if (!isAuthReady || userDataLoading) {
     return <FullLoading />
   }
-
+  if (isPrivate) {
+    return <Layout>{children}</Layout>
+  }
   return children
 }
