073fa6019fb5b2a4e340acc31e1a6fc174645f04
devRabbani <dev.golamrabbani@gmail.com>
2023-06-20 23:36:58 +0530

    👌 IMPROVE: profile votes count added with topbar

--- components/ProfileComponents/infoPage/index.jsx
+++ components/ProfileComponents/infoPage/index.jsx
@@ -1,5 +1,50 @@
+import Image from 'next/image'
 import s from './infoPage.module.css'
+import noUserImg from '@/assets/user.svg'
+import Button from '@/components/button'
+import VotesBar from '../votesBar'
 
 export default function InfoPage() {
-  return <h2>This Is Info</h2>
+  return (
+    <div className={s.infoPageWrapper}>
+      <div className={`${s.infoPage} wrapper`}>
+        <div className={s.userImgNameWrapper}>
+          <div className={s.userVotesDiv}>
+            <div className={s.userImg}>
+              <Image src={noUserImg} fill alt="User Img" />
+            </div>
+            <div className={s.votesMenuDiv}>
+              <VotesBar />
+              <div className={s.profileBtnDiv}>
+                <button>Edit Profile</button>
+              </div>
+            </div>
+          </div>
+
+          <div className={s.userName}>
+            <h3 className={s.name}>Golam Rabbani</h3>
+            <p className={s.designation}>Full Stack Developer</p>
+            <p className={s.place}>Dhubri,India</p>
+          </div>
+        </div>
+        <div className={s.socialLinksWrapper}>
+          <a href="#" className={s.socialLink}></a>
+          <a href="#" className={s.socialLink}></a>
+          <a href="#" className={s.socialLink}></a>
+          <a href="#" className={s.socialLink}></a>
+          <a href="#" className={s.socialLink}></a>
+        </div>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
+          sit veniam tempore aut adipisci consequuntur, non perferendis
+          reprehenderit amet nisi blanditiis aspernatur quaerat dolorem magni
+          molestiae vitae, itaque ipsam impedit.
+        </p>
+        <div className="btnDiv">
+          <Button>View Resume</Button>
+          <Button>See Portfolio</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
--- components/ProfileComponents/infoTop/index.jsx
+++ components/ProfileComponents/infoTop/index.jsx
@@ -1,10 +1,25 @@
+import Image from 'next/image'
 import s from './infoTop.module.css'
+import noUserImg from '@/assets/user.svg'
+import Button from '@/components/button'
+import VotesBar from '../votesBar'
 
 export default function InfoTop() {
   return (
     <div className={s.infoTopWrapper}>
       <div className={`${s.infoTop} wrapper`}>
-        <h2>This is Info</h2>
+        <div className={s.userImg}>
+          <Image src={noUserImg} fill alt="User Img" />
+        </div>
+        <div className={s.userNameWrapper}>
+          <div>
+            <h3 className={s.name}>Golam Rabbani</h3>
+            <p className={s.designation}>Full Stack Developer</p>
+            <p className={s.place}>Dhubri,India</p>
+          </div>
+
+          <VotesBar />
+        </div>
       </div>
     </div>
   )
