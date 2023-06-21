8a24ddaacee24af115805501832ee46f3f5098b6
devRabbani <dev.golamrabbani@gmail.com>
2023-06-22 00:43:33 +0530

    👌 IMPROVE: profile info page improving

--- components/ProfileComponents/infoPage/index.jsx
+++ components/ProfileComponents/infoPage/index.jsx
@@ -3,31 +3,46 @@ import s from './infoPage.module.css'
 import noUserImg from '@/assets/user.svg'
 import Button from '@/components/button'
 import VotesBar from '../votesBar'
+import {
+  RiBookmarkLine,
+  RiFileTextFill,
+  RiFileTextLine,
+  RiFolderOpenLine,
+  RiShareFill,
+} from 'react-icons/ri'
+import { FaRegFileAlt } from 'react-icons/fa'
+import { MdDescription } from 'react-icons/md'
+import { IoIosImages } from 'react-icons/io'
 
 export default function InfoPage() {
   return (
     <div className={s.infoPageWrapper}>
       <div className={`${s.infoPage} wrapper`}>
-        <div className={s.userImgNameWrapper}>
-          <div className={s.userVotesDiv}>
-            <div className={s.userImg}>
-              <Image src={noUserImg} fill alt="User Img" />
-            </div>
-            <div className={s.votesMenuDiv}>
-              <VotesBar />
-              <div className={s.profileBtnDiv}>
-                <button>Edit Profile</button>
-              </div>
-            </div>
+        <div className={s.imgVotesDiv}>
+          <div className={s.userImg}>
+            <Image src={noUserImg} fill alt="User Img" />
           </div>
-
-          <div className={s.userName}>
-            <h3 className={s.name}>Golam Rabbani</h3>
-            <p className={s.designation}>Full Stack Developer</p>
-            <p className={s.place}>Dhubri,India</p>
-          </div>
+          {/* <VotesBar /> */}
+          {/* <div className={s.votesMenuDiv}>
+            <VotesBar />
+            <div className={s.profileBtnDiv}>
+              <button className={s.saveBtn}>
+                Save <RiBookmarkLine />
+              </button>
+              <button className={s.shareBtn}>
+                <RiShareFill />
+              </button>
+            </div>
+          </div> */}
         </div>
 
+        <div className={s.userName}>
+          <h3 className={s.name}>Golam Rabbani</h3>
+          <p className={s.designation}>Full Stack Developer</p>
+          <p className={s.place}>Dhubri,India</p>
+        </div>
+        <VotesBar />
+        <h4 className="subHeader">Links</h4>
         <div className={s.socialLinksWrapper}>
           <a href="#" className={s.socialLink}></a>
           <a href="#" className={s.socialLink}></a>
@@ -35,16 +50,36 @@ export default function InfoPage() {
           <a href="#" className={s.socialLink}></a>
           <a href="#" className={s.socialLink}></a>
         </div>
-        <h4>Bio</h4>
-        <p>
+        <h4 className="subHeader">Bio</h4>
+        <p className={s.bio}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Inventore
           sit veniam tempore aut adipisci consequuntur, non perferendis
           reprehenderit amet nisi blanditiis aspernatur quaerat dolorem magni
           molestiae vitae, itaque ipsam impedit.
         </p>
         <div className="btnDiv">
-          <Button>View Resume</Button>
-          <Button>See Portfolio</Button>
+          <a
+            className={s.resumeBtn}
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Resume <RiFileTextFill />
+          </a>
+          <a
+            className={s.portfolioBtn}
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Portfolio <IoIosImages />
+          </a>
+          {/* <Button variant="border" src="#">
+            Resume <RiFileTextFill />
+          </Button> */}
+          {/* <Button src="#">
+            Portfolio <IoIosImages />
+          </Button> */}
         </div>
       </div>
     </div>
